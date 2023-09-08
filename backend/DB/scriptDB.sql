7ab89842fcb4bd12aed993fad6b3a4aa1f8433fe
jairoDGM <jairo.danielgm@gmail.com>
2023-09-08 13:52:31 -0600

    correccion error de sintaxy en script

--- backend/DB/scriptDB.sql
+++ backend/DB/scriptDB.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE mallCompass;
 USE mallCompass;
 
---CREACION DE TABLAS DE ENTIDAD
-
 CREATE TABLE usuario_admin_cc(
 		fecha_nacimiento date,
-		id_usuarioCC integer NOT NULL AUTO_INCREMENT,
+		id_usuarioCC integer NOT NULL,
 		nombre char(50),
 		apellido char(50),
 		contrasena char(50),
@@ -16,7 +14,7 @@ CREATE TABLE usuario_admin_cc(
 );
 
 CREATE TABLE usuario_admin_tienda(
-		id_usuarioTienda integer NOT NULL AUTO_INCREMENT,
+		id_usuarioTienda integer NOT NULL,
 		nombre char(50),
 		apellido char(50),
 		contrasena char(50),
@@ -31,7 +29,7 @@ CREATE TABLE usuario_final(
 		contrasena char(50),
 		apellido char(50),
 		nombre char(50),
-		id_usuarioFinal integer NOT NULL AUTO_INCREMENT,
+		id_usuarioFinal integer NOT NULL,
 		correo char(50),
 		telefono char(50),
 		imagen char(225),
@@ -40,24 +38,24 @@ CREATE TABLE usuario_final(
 );
 
 CREATE TABLE centro_comercial(
-		estado_cuenta boolean,
+		estado_cuenta char(25),
 		nombreCC char(50),
 		longitud_latitud char(255),
 		imagen char(255),
 		telefonoCC char(50),
 		correo char(50),
-		id_centroComercial integer NOT NULL AUTO_INCREMENT,
+		id_centroComercial integer NOT NULL,
 		direccion char(225),
 		PRIMARY KEY (id_centroComercial)
 );
 
 CREATE TABLE tiendas(
-		id_tienda integer NOT NULL AUTO_INCREMENT,
+		id_tienda integer NOT NULL,
 		nombreTienda char(50),
 		imagen char(255),
 		telefono char(50),
 		numeroLocal char(50),
-		estado_cuenta boolean,
+		estado_cuenta char(25),
 		categoriaTienda char(50),
 		correo char(50),
 		PRIMARY KEY (id_tienda)
@@ -66,7 +64,7 @@ CREATE TABLE tiendas(
 CREATE TABLE Publicaciones(
 		descripcion char(255),
     actividad char(25),
-		id_post integer NOT NULL AUTO_INCREMENT,
+		id_post integer NOT NULL,
     vigencia_inicio datetime,
     vigencia_final datetime,
 		imagen char(255),
@@ -77,22 +75,22 @@ CREATE TABLE Publicaciones(
 CREATE TABLE promociones(
 		descripcion char(255),
     categoria char(25),
-		id_promocion integer NOT NULL AUTO_INCREMENT,
+		id_promocion integer NOT NULL,
     vigencia_inicio datetime,
     vigencia_final datetime,
     timer time,
     cantidad integer,
 		imagen char(255),
 		qr char(255),
-    precio float
+    precio float,
 		PRIMARY KEY (id_promocion)
 );
 
 CREATE TABLE productos(
     existencia integer,
-		id_producto integer NOT NULL AUTO_INCREMENT,
+		id_producto integer,
 		descripcion char(255),
-    estado_producto boolean,
+    estado_producto char(25),
     categoria char(25),
     nombre char(50),
     precio float,
@@ -102,15 +100,15 @@ CREATE TABLE productos(
 );
 
 CREATE TABLE preferencias(
-		id_gusto integer NOT NULL AUTO_INCREMENT,
+		id_gusto integer NOT NULL,
     categoria_actividades char(25),
     categoria_tiendas char(25),
     categoria_productos char(25),
 		PRIMARY KEY (id_gusto)
 );
-------------------------------------------------------
+/*------------------------------------------------------
 --CREACION DE TABLAS DE RELACION
--- ------>relaciones de usuarios
+-- ------>relaciones de usuarios*/
 CREATE TABLE rel_user_cc(
     id_usuarioCC integer,
     id_centroComercial integer,
@@ -153,14 +151,14 @@ CREATE TABLE rel_user_promociones(
     FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion)
 );
 
-----------------------------------------------
--- ------>relaciones varias
+
+/*relaciones varias*/
 CREATE TABLE rel_cc_tiendas(
     id_centroComercial integer,
     id_tienda integer,
     PRIMARY KEY (id_centroComercial,id_tienda),
     FOREIGN KEY (id_centroComercial) REFERENCES centro_comercial(id_centroComercial),
-    FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
+    FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda)
 );
 
 CREATE TABLE rel_cc_publicaciones(
@@ -169,16 +167,16 @@ CREATE TABLE rel_cc_publicaciones(
     categoria char(25),
     PRIMARY KEY (id_post,id_centroComercial),
     FOREIGN KEY (id_post) REFERENCES Publicaciones(id_post),
-    FOREIGN KEY (id_centroComercial) REFERENCES centro_comercial(id_centroComercial),
+    FOREIGN KEY (id_centroComercial) REFERENCES centro_comercial(id_centroComercial)
 );
 
-CREATE TABLE rel_cc_tiendas(
+CREATE TABLE rel_tiendas_publicaciones(
     id_post integer,
     id_tienda integer,
-    categoria char(25).
+    categoria char(25),
     PRIMARY KEY (id_post,id_tienda),
-    FOREIGN KEY (id_post) REFERENCES Publicaciones(id_post)
-    FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
+    FOREIGN KEY (id_post) REFERENCES Publicaciones(id_post),
+    FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda)
 );
 
 CREATE TABLE rel_tiendas_promociones(
@@ -187,7 +185,7 @@ CREATE TABLE rel_tiendas_promociones(
     categoria char(25),
     PRIMARY KEY (id_tienda,id_promocion),
     FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
-    FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion),
+    FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion)
 );
 
 CREATE TABLE rel_tiendas_productos(
@@ -195,5 +193,5 @@ CREATE TABLE rel_tiendas_productos(
     id_producto integer,
     PRIMARY KEY (id_tienda,id_producto),
     FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
-    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
