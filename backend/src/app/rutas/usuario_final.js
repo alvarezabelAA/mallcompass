dc20a546267ae0e1cb33f68db3323e600e171092
jairoDGM <jairo.danielgm@gmail.com>
2023-09-18 16:08:02 -0600

    implementacion cors

--- backend/src/app/rutas/usuario_final.js
+++ backend/src/app/rutas/usuario_final.js
@@ -1,9 +1,20 @@
 const conn = require('../../config/database');
 const bcrypt = require('bcrypt'); /*funcion hash*/
+const cors = require('cors');
 
 module.exports = (app) => {
+
+    
+
+    /*const corsOptions = {
+        origin: '*', // Reemplaza con el dominio del frontend
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: ['Content-Type','Authorization'],
+      };*/
+
+    app.options('/usuario/final/consulta', cors());
     /*CONSULTA GENERAL DATOS USUARIOS*/
-    app.get('/usuario/final/consulta',(req, res)=>{
+    app.get('/usuario/final/consulta', cors(),(req, res)=>{
         console.log("ejecucion metodo GET");
         let query = "SELECT * FROM usuario_final";
         conn.query(query, (error, filas) => {
@@ -16,8 +27,9 @@ module.exports = (app) => {
     });
 
 
+    app.options('/usuario/final/consulta/:id_usuarioFinal', cors());
     /*CONSULTA DATOS USUARIO */
-    app.get('/usuario/final/consulta/:id_usuarioFinal',(req, res)=>{
+    app.get('/usuario/final/consulta/:id_usuarioFinal', cors(),(req, res)=>{
         console.log("ejecucion metodo GET");
         let query = `SELECT * FROM usuario_final WHERE id_usuarioFinal = ${req.params.id_usuarioFinal}`;
         conn.query(query, (error, filas) => {
@@ -30,8 +42,9 @@ module.exports = (app) => {
     });
 
 
+    app.options('/usuario/final/login/:correo/:contrasena', cors());
     /*LOGIN USUARIOS FINALES */
-    app.get('/usuario/final/login/:correo/:contrasena',(req, res)=>{
+    app.get('/usuario/final/login/:correo/:contrasena', cors(),(req, res)=>{
         console.log("ejecucion metodo GET");
 
 
@@ -65,31 +78,33 @@ module.exports = (app) => {
     });
 
 
+    app.options('/usuario/final/registro', cors());
     /* REGISTRO DATOS USUARIO*/
-    app.post('/usuario/final/registro',(req,res)=>{
+    app.post('/usuario/final/registro', cors(),(req, res) => {
         console.log("ejecucion metodo POST");
-        if(!req.body.contrasena || !req.body.apellido || !req.body.nombre || !req.body.correo || !req.body.telefono || !req.body.imagen || !req.body.fecha_nacimiento){
-            res.json({status: 0, mensaje: "error datos enviados", descripcion:"algun campo enviado se encuentra vacio"});
+        if (!req.body.contrasena || !req.body.apellido || !req.body.nombre || !req.body.correo || !req.body.telefono || !req.body.imagen || !req.body.fecha_nacimiento) {
+        res.json({ status: 0, mensaje: "error datos enviados", descripcion: "algun campo enviado se encuentra vacío" });
+        return;
+        }
+    
+        bcrypt.hash(req.body.contrasena, 10, (err, hash) => {
+        if (err) {
+            console.error('Error al hashear la contraseña:', err);
             return;
         }
-
-        bcrypt.hash(req.body.contrasena, 10, (err, hash) => {
-            if (err) {
-              console.error('Error al hashear la contraseña:', err);
-              return;
+        console.log('Contraseña hasheada:', hash);
+    
+        let query = `INSERT INTO usuario_final(contrasena, apellido, nombre, correo, telefono, imagen, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        const values = [hash, req.body.apellido, req.body.nombre, req.body.correo, req.body.telefono, req.body.imagen, req.body.fecha_nacimiento];
+    
+        conn.query(query, values, (error, filas) => {
+            if (error) {
+            res.json({ status: 0, mensaje: "error en DB", datos: error });
+            } else {
+            res.json({ status: 1, mensaje: "datos insertados en DB", datos: [] });
             }
-            console.log('Contraseña hasheada:', hash);
-
-            let query = `INSERT INTO usuario_final(contrasena, apellido, nombre, correo, telefono, imagen, fecha_nacimiento) VALUES ('${hash}','${req.body.apellido}','${req.body.nombre}','${req.body.correo}','${req.body.telefono}','${req.body.imagen}','${req.body.fecha_nacimiento}')`;
-            conn.query(query, (error, filas)=>{
-                if(error){
-                    res.json({status: 0, mensaje: "error en DB", datos:error});
-                }else{
-                    res.json({status: 1, mensaje: "datos insertados en DB", datos: []});
-                }
-            });
         });
-
+        });
     });
 
 
