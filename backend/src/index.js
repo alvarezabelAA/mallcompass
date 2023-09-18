ff2b760ef13283814ae6f90585b2f2f96202c670
jairoDGM <jairo.danielgm@gmail.com>
2023-09-18 10:58:27 -0600

    manejo de rutas erroneas

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,5 +3,9 @@ require('./app/rutas/usuario_final')(app);
 
 app.listen(app.get('puerto'), () => console.log(`Servidor Corriendo en puerto ${app.get('puerto')}`));
 
+app.use((req,res)=>{
+    res.status(404).json({status: 1, mensaje: "ruta o endpoint erroneo", datos: []})
+})
+
 /*ruta local de prueba http://localhost:4044 */
 /*ejemplo de ruta con ngrok https://405c-186-151-141-111.ngrok.io/usuario/final */
