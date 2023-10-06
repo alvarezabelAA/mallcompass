c5f3dbd4c74646be2a0c61ec2182b72d15b128be
jairoDGM <jairo.danielgm@gmail.com>
2023-10-06 03:09:06 -0600

    endpoint modificaicon datos de centros comerciales

--- backend/src/app/rutas/centro_comercial.js
+++ backend/src/app/rutas/centro_comercial.js
@@ -95,4 +95,36 @@ module.exports = (app) => {
         });
 
     });
+
+
+
+    /*MODIFICACION DATOS DE UN SOLO CC */
+    app.options('/centroComercial/consulta', cors());
+    app.put('/centroComercial/consulta', cors(),(req, res)=>{
+        console.log("ejecucion metodo GET");
+        let query = `SELECT * FROM logintokens WHERE token = '${req.query.token}'`;
+        conn.query(query, (error, filas) => {
+          if(error){
+            console.log("No se encontró el token");
+          }else{
+            if(filas.length == 0){
+              console.log("consulta sin elementos");
+              res.json({ status: 1, mensaje: "error de token", datos: filas });
+            }else{
+              console.log("encontró el token");
+              let query = `UPDATE centro_comercial SET estado_cuenta=?,nombreCC=?,longitud=?,latitud=?,imagen=?,telefonoCC=?,correo=?,direccion=? WHERE id_centroComercial = '${req.query.idComercial}'`;
+              const { estado_cuenta, nombreCC, longitud, latitud, imagen, telefonoCC, correo, direccion } = req.body;
+              const values = [estado_cuenta, nombreCC, longitud, latitud, imagen, telefonoCC, correo, direccion];
+              conn.query(query, values, (error, filas) => {
+              if(error){
+                  res.json({ status: 0, mensaje: "error en DB", datos:error });
+              }else{
+                  res.json({ status: 1, mensaje: "datos actualizados en DB", datos: filas });
+              }
+              });
+            }
+          }
+        });
+
+    });
 }
