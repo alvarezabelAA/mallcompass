4a3fbae4255e222c0d10c83457acd2a0e6384cd4
jairoDGM <jairo.danielgm@gmail.com>
2023-10-13 02:52:52 -0600

    implementacion lista especia CC

--- backend/src/app/rutas/centro_comercial.js
+++ backend/src/app/rutas/centro_comercial.js
@@ -121,11 +121,11 @@ module.exports = (app) => {
 
               let query = `SELECT * FROM centro_comercial WHERE id_centroComercial = '${req.query.idComercial}'`;
               conn.query(query, (error, filas) => {
-              if(error){
-                  res.json({ status: 0, mensaje: "error en DB", datos:error });
-              }else{
-                  res.json({ status: 1, mensaje: "datos obtenidos", datos: filas });
-              }
+                if(error){
+                    res.json({ status: 0, mensaje: "error en DB", datos:error });
+                }else{
+                    res.json({ status: 1, mensaje: "datos obtenidos", datos: filas });
+                }
               });
             }
           }
@@ -199,4 +199,108 @@ module.exports = (app) => {
         });
 
     });
+
+
+
+
+
+    /*Lista de CC especial*/
+    app.options('/centroComercial/lista', cors());
+    app.get('/centroComercial/consulta', cors(),(req, res)=>{
+        console.log("ejecucion metodo GET");
+        let query = `SELECT * FROM logintokens WHERE token = '${req.query.token}'`;
+        conn.query(query, (error, filas) => {
+          if(error){
+            console.log("No se encontró el token");
+          }else{
+            if(filas.length == 0){
+              console.log("consulta sin elementos");
+              res.json({ status: 1, mensaje: "error de token", datos: filas });
+            }else{
+              console.log("encontro el token");
+
+              let query2 = `SELECT centro_comercial.*,
+              IFNULL(count_tiendas, 0) AS cantidad_tiendas,
+              IFNULL(count_usuarios, 0) AS cantidad_usuarios
+              FROM centro_comercial
+              LEFT JOIN (
+                  SELECT id_centroComercial, COUNT(*) AS count_tiendas
+                  FROM rel_cc_tiendas
+                  GROUP BY id_centroComercial
+              ) AS tiendas_count 
+              ON centro_comercial.id_centroComercial = tiendas_count.id_centroComercial
+              
+              LEFT JOIN (
+                  SELECT id_centroComercial, COUNT(*) AS count_usuarios
+                  FROM rel_user_cc
+                  GROUP BY id_centroComercial
+              ) AS usuarios_count 
+              ON centro_comercial.id_centroComercial = usuarios_count.id_centroComercial
+              WHERE centro_comercial.estado_cuenta = 'A'
+              HAVING cantidad_usuarios > 0
+              LIMIT 0, 25`;
+              conn.query(query2, (error2, filas2) => {
+                if(error2){
+                    res.json({ status: 0, mensaje: "error en DB", datos:error2 });
+                }else{
+                    res.json({ status: 1, mensaje: "datos obtenidos", datos: filas2 });
+                }
+              });
+            }
+          }
+        });
+      });
+
+
+      /*Lista de CC usuarios especial*/
+    app.options('/centroComercial/listaUsuarios', cors());
+    app.get('/centroComercial/consulta', cors(),(req, res)=>{
+        console.log("ejecucion metodo GET");
+        let query = `SELECT * FROM logintokens WHERE token = '${req.query.token}'`;
+        conn.query(query, (error, filas) => {
+          if(error){
+            console.log("No se encontró el token");
+          }else{
+            if(filas.length == 0){
+              console.log("consulta sin elementos");
+              res.json({ status: 1, mensaje: "error de token", datos: filas });
+            }else{
+              console.log("encontro el token");
+
+
+              let query2 = `SELECT centro_comercial.*,
+              IFNULL(count_tiendas, 0) AS cantidad_tiendas,
+              IFNULL(count_usuarios, 0) AS cantidad_usuarios
+              FROM centro_comercial
+              LEFT JOIN (
+                  SELECT id_centroComercial, COUNT(*) AS count_tiendas
+                  FROM rel_cc_tiendas
+                  GROUP BY id_centroComercial
+              ) AS tiendas_count 
+              ON centro_comercial.id_centroComercial = tiendas_count.id_centroComercial
+              
+              LEFT JOIN (
+                  SELECT id_centroComercial, COUNT(*) AS count_usuarios
+                  FROM rel_user_cc
+                  WHERE rel_user_cc.id_usuario = ${req.query.id_usuario}
+                  GROUP BY id_centroComercial
+              ) AS usuarios_count 
+              ON centro_comercial.id_centroComercial = usuarios_count.id_centroComercial
+              WHERE centro_comercial.estado_cuenta = 'A'
+              HAVING cantidad_usuarios > 0
+              LIMIT 0, 25`;
+              conn.query(query2, (error2, filas2) => {
+                if(error2){
+                    res.json({ status: 0, mensaje: "error en DB", datos:error2 });
+                }else{
+                    res.json({ status: 1, mensaje: "datos obtenidos", datos: filas2 });
+                }
+              });
+            }
+          }
+        });
+      });
+
+
+
 }
