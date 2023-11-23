2f26d7a5444d2bbc4c76945dea41a92f0a7c4029
jairoDGM <jairo.danielgm@gmail.com>
2023-11-23 05:20:22 -0600

    implementacion promociones v1

--- backend/src/app/rutas/promociones.js
+++ backend/src/app/rutas/promociones.js
@@ -40,4 +40,73 @@ module.exports = (app) => {
             }
         });
     });
+
+
+     /*CONSULTA PROMO DE TIENDA */
+     app.options('/promociones/publicar', cors());
+     app.get('/promociones/publicar', cors(),(req, res)=>{
+         console.log("ejecucion metodo GET");
+         let query = `SELECT * FROM logintokens WHERE token = '${req.query.tokenSesion}'`;
+         conn.query(query, (error, filas) => {
+         if(error){
+             console.log("No se encontró el token");
+         }else{
+             if(filas.length == 0){
+                 console.log("consulta sin elementos");
+                 res.json({ status: 0, mensaje: "error de token", datos: filas });
+             }else{
+                 console.log("encontro el token");
+                 let query = `SELECT * FROM promociones INNER JOIN rel_tiendas_promociones ON promociones.id_promocion =rel_tiendas_promociones.id_promocion WHERE id_tienda = '${req.query.id_tienda}'`;
+                 conn.query(query, (error, filas) => {
+                     if(error){
+                         res.json({ status: 0, mensaje: "error en DB", datos:error });
+                     }else{
+                         if(filas.length == 0){
+                             console.log("consulta sin elementos");
+                             res.json({ status: 0, mensaje: "no hay publicaciones", datos: filas });
+                         }else{
+                             res.json({ status: 1, mensaje: "datos obtenidos", datos: filas });
+                         }
+                     }
+                 });
+             }
+         }
+         });
+     });
+
+
+     /*CONSULTA PROMO DE TODAS LAS TIENDAS */
+     app.options('/promociones/publicar/general', cors());
+     app.get('/promociones/publicar/general', cors(),(req, res)=>{
+         console.log("ejecucion metodo GET");
+         let query = `SELECT * FROM logintokens WHERE token = '${req.query.tokenSesion}'`;
+         conn.query(query, (error, filas) => {
+         if(error){
+             console.log("No se encontró el token");
+         }else{
+             if(filas.length == 0){
+                 console.log("consulta sin elementos");
+                 res.json({ status: 0, mensaje: "error de token", datos: filas });
+             }else{
+                 console.log("encontro el token");
+                 let query = `SELECT * FROM promociones INNER JOIN rel_tiendas_promociones ON promociones.id_promocion =rel_tiendas_promociones.id_promocion`;
+                 conn.query(query, (error, filas) => {
+                     if(error){
+                         res.json({ status: 0, mensaje: "error en DB", datos:error });
+                     }else{
+                         if(filas.length == 0){
+                             console.log("consulta sin elementos");
+                             res.json({ status: 0, mensaje: "no hay publicaciones", datos: filas });
+                         }else{
+                             res.json({ status: 1, mensaje: "datos obtenidos", datos: filas });
+                         }
+                     }
+                 });
+             }
+         }
+         });
+     });
+
+
+
 }
